ec53232739b32f71060a7a12f8f0e2fc4fbf048b
simpsons310 <giangnguyen1997@gmail.com>
2021-07-15 01:13:06 +0700

    Higher Order Components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed}/>
-            </div>
+            </Aux>
         );
     }
 }
--- src/container/App.js
+++ src/container/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component
 {
@@ -87,7 +88,7 @@ class App extends Component
         }
 
         return (
-                <div className={classes.App}>
+                <WithClass classes={classes.App}>
                     <button 
                         onClick={() => {this.setState({ showCockpit: false })}}
                     >
@@ -101,7 +102,7 @@ class App extends Component
                             clicked={this.togglePersonsHandler} />
                     ) : null}
                     {persons}
-                </div>
+                </WithClass>
         );
     }
 }

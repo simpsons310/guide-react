7d180300c5749e93e4a44d0c2a47b38d34de97c9
GiangNH <giangnh@hblab.vn>
2021-08-31 23:17:42 +0700

    add burger builder container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/auxiliary';
+
+class BurgerBuilder extends Component
+{
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Burger Builder</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder;

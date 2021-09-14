7327d90fc643c4f9e4ee5ca4320e42b50e29e5ce
GiangNH <giangnh@hblab.vn>
2021-09-14 23:13:00 +0700

    adding the price to the order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -97,6 +97,7 @@ class BurgerBuilder extends Component
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary
+                        price={this.state.totalPrice}
                         ingredients={this.state.ingredients}
                         purchaseCanceled={this.purchaseCancelHandler}
                         purchaseContinued={this.purchaseContinueHHandler}

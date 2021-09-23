323bcce6076598425d06c81dcda471540feec3c2
GiangNH <giangnh@hblab.vn>
2021-09-23 21:35:55 +0700

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
-class App extends Component
-{    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+import Blog from './containers/Blog/Blog';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Blog />
+      </div>
+    );
+  }
 }
 
 export default App;

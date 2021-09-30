29a0e7d435dff67f6565adec8c0a89ec54b0dca4
GiangNH <giangnh@hblab.vn>
2021-09-30 22:54:33 +0700

    sending a post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -21,7 +22,7 @@ class BurgerBuilder extends Component
             cheese: 0,
             meat: 0
         },
-        totalPrice: 0,
+        totalPrice: 0.0,
         purchaseable: false,
         purchasing: false
     }
@@ -47,6 +48,7 @@ class BurgerBuilder extends Component
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
+        console.log(newPrice);
         this.setState({
             totalPrice: newPrice,
             ingredients: updatedIngredients
@@ -83,7 +85,25 @@ class BurgerBuilder extends Component
     }
 
     purchaseContinueHHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Max',
+                address: {
+                    street: 'Test 1',
+                    zipCode: '1234',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {

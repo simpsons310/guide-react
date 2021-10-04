6036eb42ea8e758d131176c78d27e8b5d8aa238d
GiangNH <giangnh@hblab.vn>
2021-10-05 00:30:22 +0700

    redirectingrequests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Blog.css';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from  './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
@@ -37,6 +37,9 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost}/>
                     <Route path="/posts" component={Posts}/>
+                    <Redirect from="/" to="/posts"/>
+                    {/* <Route path="/" component={Posts}/> */}
+
                 </Switch>
             </div>
         );

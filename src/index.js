9ca01f5434ad6a2f602339bdd7ee9a9b475399a9
GiangNH <giangnh@hblab.vn>
2021-10-13 23:01:01 +0700

    connecting the store to react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
@@ -9,7 +10,12 @@ import reducer from './store/reducer';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<React.StrictMode><App appTitle="Person Manager"/></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}><App/></Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

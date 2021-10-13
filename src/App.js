df5a8ba69e6053b6b930d66f5d2a28b60ec79af0
GiangNH <giangnh@hblab.vn>
2021-10-13 22:25:07 +0700

    initialize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
-class App extends Component
-{    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+import Counter from './containers/Counter/Counter';
+import classes from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+       <Counter />
+      </div>
+    );
+  }
 }
 
 export default App;

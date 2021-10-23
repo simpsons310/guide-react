7f4c31cbdfd406737e7e604c3c992a74c5d78bd7
giang nguyen <giangnguyen1997@gmail.com>
2021-10-23 21:59:42 +0700

    creating action creators

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import reducer from './store/reducer/burgerBulider';
+import burgerBuilderReducer from './store/reducer/burgerBuilder';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <React.StrictMode>

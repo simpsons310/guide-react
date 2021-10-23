eed01bf985c6e134608d16e1395fd9c41057ce61
giang nguyen <giangnguyen1997@gmail.com>
2021-10-23 21:59:42 +0700

    creating action creators

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,8 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-order';
-import * as actionTypes from '../../store/actions/actionTypes';
+// import * as burgerBuliderActions from '../../store/actions';
+import * as burgerBuliderActions from '../../store/actions/index';
 
 class BurgerBuilder extends Component
 {
@@ -106,8 +107,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingredientName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingredientName}),
-        onIngredientRemoved: (ingredientName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingredientName})
+        onIngredientAdded: (ingredientName) => dispatch(burgerBuliderActions.addIngredient(ingredientName)),
+        onIngredientRemoved: (ingredientName) => dispatch(burgerBuliderActions.removeIngredient(ingredientName))
     }
 }
 

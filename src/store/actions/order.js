4af9baa4c84bc8b2ed11698495b612268df73c09
giang nguyen <giangnguyen1997@gmail.com>
2021-10-24 16:27:24 +0700

    fetching orders via redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,7 +27,6 @@ export const purchaseBurger = (orderData) => {
         dispatch(purchaseBurgerStart());
         axios.post('/orders.json', orderData)
             .then(response => {
-                console.log(response.data);
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData))
             })
             .catch(error => {
@@ -40,4 +39,44 @@ export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
     };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    };
+};
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    };
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart);
+        axios.get('/orders.json')
+            .then(response => {
+                const fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            })
+            .catch(error => {
+                dispatch(fetchOrdersFail(error));
+            });
+    }
 };

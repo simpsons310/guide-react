47d5cbb459a28fc47c8555e5bc780ac13e8722fb
giang nguyen <giangnguyen1997@gmail.com>
2021-11-01 22:43:53 +0700

    redirecting the user to the checkout page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -43,6 +43,12 @@ class Auth extends Component {
         isSignup: true,
     }
 
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
+    }
+
     checkValidity(value, rules) {
         let isValid = true;
         if (!rules) {
@@ -131,7 +137,7 @@ class Auth extends Component {
 
         let authRedirect = null;
         if (this.props.isAuthenticatd) {
-            authRedirect = <Redirect to="/" />;
+            authRedirect = <Redirect to={this.props.authRedirectPath} />;
         }
 
         return (
@@ -157,13 +163,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAuthenticatd: state.auth.token !== null
+        isAuthenticatd: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
 

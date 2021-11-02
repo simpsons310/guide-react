dac47a800d804693c016e97c1d24e61b6427d596
giang nguyen <giangnguyen1997@gmail.com>
2021-11-02 23:27:03 +0700

    sharing the validation method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import classes from './ContactData.css';
 import axios from '../../../axios-order';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
     state = {
@@ -97,27 +97,6 @@ class ContactData extends Component {
         formIsValid: false
     };
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        return isValid;
-    };
-
     orderHandler = (event) => {
         event.preventDefault();
         const formData = {};
@@ -137,7 +116,7 @@ class ContactData extends Component {
     inputChangedHandler = (event, inputIdentify) => {
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentify], {
             value: event.target.value,
-            valid: this.checkValidity(updatedFormElement.value, updatedFormElement.validation),
+            valid: checkValidity(event.target.value, this.state.orderForm[inputIdentify].validation),
             touched: true
         });
         const updatedOrderForm = updateObject(this.state.orderForm, {

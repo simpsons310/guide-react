5a0a216a361b0246482f2af490ba27428920c934
giang nguyen <giangnguyen1997@gmail.com>
2021-11-07 23:29:40 +0700

    a special lifecycle hook

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b9fb7cee2641f47"; }
+/******/ 		__webpack_require__.h = function() { return "45e31ddc91ac90dc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
     <div>
-        <h1>The Auth Main Page</h1>
+        <h1>The Auth Main Page of {props.appName}</h1>
         <User name="Max" age={28} />
     </div>
 );
+
+authIndexPage.getInitialProps = (context) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({appName: 'Super App (Auth)'});
+        }, 1000);
+    });
+    return promise;
+};
 
 export default authIndexPage;

776cc01ae8cd11527de28f882ce217843fd5a51f
giang nguyen <giangnguyen1997@gmail.com>
2021-11-15 23:19:16 +0700

    adding array destruction

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 
 const Todo = props => {
-    const inputState = useState('');
+    const [todoName, setTodoName] = useState('');
 
     const inputChangeHandler = (event) => {
-        inputState[1](event.target.value);
+        setTodoName(event.target.value);
     };
 
     return (
         <React.Fragment>
-            <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={inputState[0]} />
+            <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoName} />
             <button type="button">Add</button>
             <ul/>
         </React.Fragment>

8751e796a174de714a2a5d99226a43758a78b656
GiangNH <giangnh@hblab.vn>
2021-11-15 15:50:54 +0700

    handling auto sign in with a saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -50,7 +50,7 @@ export const auth = (email, password, isSignUp) => {
         email: email,
         password: password,
         isSignUp: isSignUp
-    }
+    };
 };
 
 export const setAuthRedirectPath = (path) => {
@@ -61,20 +61,7 @@ export const setAuthRedirectPath = (path) => {
 };
 
 export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            dispatch(logout());
-        } else {
-            const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if (expirationDate <= new Date()) {
-                dispatch(logout());
-            } else {
-                const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
-            }
-        }
-
+    return {
+        type: actionTypes.AUTH_CHECK_STATE
     };
 };

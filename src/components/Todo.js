ee9e26007cc28ee4fe12b1d405c15c98058251f1
giang nguyen <giangnguyen1997@gmail.com>
2021-11-15 23:33:02 +0700

    using one state instead

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,22 @@ import React, { useState } from 'react';
 const Todo = props => {
     const [todoName, setTodoName] = useState('');
     const [todoList, setTodoList] = useState([]);
+    // const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
 
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
+        // setTodoState({
+        //     userInput: event.target.value,
+        //     todoList: todoState.todoList
+        // });
     };
 
     const todoAddHandler = () => {
         setTodoList(todoList.concat(todoName));
+        // setTodoState({
+        //     userInput: todoState.userInput,
+        //     todoList: todoState.todoList.concat(todoState.userInput)
+        // });
     }
 
     return (

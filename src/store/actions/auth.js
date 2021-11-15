95982d51db3a02f76ee2fc251eda80a1d9728073
GiangNH <giangnh@hblab.vn>
2021-11-15 15:13:54 +0700

    moving more logic into sagas

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -30,14 +30,19 @@ export const logout = () => {
 
     return {
         type: actionTypes.AUTH_INITIATE_LOGOUT
-    }
+    };
+}
+
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
 }
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime: expirationTime
     };
 }
 

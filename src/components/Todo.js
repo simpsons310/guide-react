fc3d969e4e3e48a5bc0226cbb3bdf079ab1f2ef9
GiangNH <giangnh@hblab.vn>
2021-11-16 18:03:23 +0700

    sending data via http

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Todo = props => {
     const [todoName, setTodoName] = useState('');
@@ -19,6 +20,13 @@ const Todo = props => {
         //     userInput: todoState.userInput,
         //     todoList: todoState.todoList.concat(todoState.userInput)
         // });
+        axios.post('https://react-guide-41949-default-rtdb.asia-southeast1.firebasedatabase.app/todos.json', {name: todoName})
+            .then(res => {
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     return (
